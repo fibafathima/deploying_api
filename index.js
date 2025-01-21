0b9ddadf197a68a6c4d7430a7d518cd9b7670657
fiba fathima <fibaaminnu@gmail.com>
2025-01-21 09:13:29 +0530

    deploying api

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { resolve } = require('path');
+
+const app = express();
+const port = 3010;
+
+app.use(express.static('static'));
+
+app.get('/', (req, res) => {
+  res.sendFile(resolve(__dirname, 'pages/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+require('dotenv').config();
+
+// Access environment variables
+const config = {
+  apiKey: process.env.API_KEY,
+  serverSecret: process.env.SERVER_SECRET,
+  isKalvian: process.env.IS_KALVIAN === 'true',
+};
+
+// Export the config object
+module.exports = config;
+
+// Log the variables (Optional for testing)
+console.log(config);
